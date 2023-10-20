ac2072270e4e5906e703f5c6bf4dcd64522450d1
Yarib Díaz <yaribdiazz@gmail.com>
2023-10-19 20:48:18 -0600

    testing cors

--- src/hooks/useGetData.js
+++ src/hooks/useGetData.js
@@ -16,11 +16,14 @@ export const useGetData = (page, triggerSearch) => {
 
   const getData = async () => {
     try {
-            const {data} = await axios(`${import.meta.env.VITE_API_CHARACTER}/?page=${currentPage}&name=${search}&status=alive`, {
-        headers: {
-          'Content-Type': 'application/json;charset=UTF-8',
-          'Access-Control-Allow-Origin': '*' // Could work and fix the previous problem, but not in all APIs
-        }})
+        //     const {data} = await axios(`${import.meta.env.VITE_API_CHARACTER}/?page=${currentPage}&name=${search}&status=alive`, {
+        // headers: {
+        //   'Content-Type': 'application/json;charset=UTF-8',
+        //   'Access-Control-Allow-Origin': '*' // Could work and fix the previous problem, but not in all APIs
+        // }})
+        const data ={
+          error: 'hay error'
+        }
 
 
       handleSetLoading(true)
